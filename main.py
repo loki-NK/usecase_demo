b2b4ea92f52f1e3e24b524547e810bd3dbb92469
KIRAN NK <loki-NK@users.noreply.github.com>
2025-02-07 19:32:15 +0530

    Update main.py
    Streamlit

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from flask import Flask, Response
+import streamlit as st
 from plivo import plivoxml
 
-app = Flask(__name__)
-
-@app.route('/waiting_room', methods=['GET'])
-def waiting_room():
+def generate_waiting_room_xml():
     response = plivoxml.ResponseElement()
     response.add(
         plivoxml.ConferenceElement(
             'Waiting Room',
             wait_sound='https://<yourdomain>.com/waitmusic/',
             enter_sound=''))
-    return Response(response.to_string(), mimetype='application/xml')
+    return response.to_string()
 
-@app.route('/operator_room', methods=['GET'])
-def operator_room():
+def generate_operator_room_xml():
     operator_response = plivoxml.ResponseElement()
     operator_response.add(
         plivoxml.ConferenceElement(
             'Waiting Room', enter_sound='', end_conference_on_exit=True))
-    return Response(operator_response.to_string(), mimetype='application/xml')
+    return operator_response.to_string()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+st.title("Plivo Conference XML Generator")
+option = st.selectbox("Choose XML to Generate:", ["Waiting Room", "Operator Room"])
+
+if option == "Waiting Room":
+    st.code(generate_waiting_room_xml(), language='xml')
+elif option == "Operator Room":
+    st.code(generate_operator_room_xml(), language='xml')
