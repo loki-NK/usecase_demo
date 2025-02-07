aab775c0ab7f98a4b8f78b1495190b7f98a06b41
KIRAN NK <loki-NK@users.noreply.github.com>
2025-02-07 19:41:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import streamlit as st
+from flask import Flask, Response
 from plivo import plivoxml
+import streamlit as st
+import threading
+
+app = Flask(__name__)
 
 def generate_waiting_room_xml():
     response = plivoxml.ResponseElement()
@@ -17,10 +21,20 @@ def generate_operator_room_xml():
             'Waiting Room', enter_sound='', end_conference_on_exit=True))
     return operator_response.to_string()
 
-st.title("Plivo Conference XML Generator")
-option = st.selectbox("Choose XML to Generate:", ["Waiting Room", "Operator Room"])
+@app.route('/waiting_room', methods=['GET'])
+def waiting_room():
+    return Response(generate_waiting_room_xml(), mimetype='application/xml')
 
-if option == "Waiting Room":
-    st.code(generate_waiting_room_xml(), language='xml')
-elif option == "Operator Room":
-    st.code(generate_operator_room_xml(), language='xml')
+@app.route('/operator_room', methods=['GET'])
+def operator_room():
+    return Response(generate_operator_room_xml(), mimetype='application/xml')
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+
+threading.Thread(target=run_flask, daemon=True).start()
+
+st.title("Plivo Conference XML Generator")
+st.write("Use the following endpoints to get conference XML:")
+st.write("[Waiting Room XML](http://localhost:5000/waiting_room)")
+st.write("[Operator Room XML](http://localhost:5000/operator_room)")
